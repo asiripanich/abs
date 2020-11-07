e2885581ef194b24843ba8ec01113c8e8c8a316c
amarin <17020181+asiripanich@users.noreply.github.com>
2020-11-07 23:20:11 +1100

    test: remove invalid tests

--- tests/testthat/test-read-tablebuilder.R
+++ tests/testthat/test-read-tablebuilder.R
@@ -2,7 +2,7 @@ test_that("read tablebuilder", {
   data_dir <- system.file("extdata", package = "abs")
   test_csv <- file.path(data_dir, "tb1.csv")
   test_cleaned_csv <- file.path(data_dir, "tb1_cleaned.csv")
-  test2_csv <- file.path(data_dir, "tb2_modified.csv")
+  # test2_csv <- file.path(data_dir, "tb2_modified.csv")
   expect_equal(abs_read_tb(test_csv), abs_read_tb(test_cleaned_csv))
   expect_equal(abs_read_tb(test_csv, exclude_total = FALSE), abs_read_tb(test_cleaned_csv, exclude_total = FALSE))
   expect_equal(abs_read_tb(test_csv, exclude_total = TRUE), abs_read_tb(test_cleaned_csv, exclude_total = TRUE))
@@ -14,6 +14,6 @@ test_that("read tablebuilder", {
     abs_read_tb(test_csv, .names = "clean") %>% names() %>% grepl(" ", .) %>% sum(),
     0
   )
-  expect_is(abs_read_tb(test2_csv), class = "data.table")
+  # expect_is(abs_read_tb(test2_csv), class = "data.table")
 })
 
